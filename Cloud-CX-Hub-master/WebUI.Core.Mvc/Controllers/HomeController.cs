ea8e2915d6df1def82e2a30e60b28d5e97d0a12c
rjerlovtdc <rjer@tdc.dk>
2025-02-18 10:42:26 +0100

    HomeController links to Overview

--- Cloud-CX-Hub-master/WebUI.Core.Mvc/Controllers/HomeController.cs
+++ Cloud-CX-Hub-master/WebUI.Core.Mvc/Controllers/HomeController.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Identity.Web;
 using WebUI.Core.Mvc.Models;
 
 namespace WebUI.Core.Mvc.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -16,8 +18,14 @@ namespace WebUI.Core.Mvc.Controllers
             _logger = logger;
         }
 
-        public IActionResult Login()
+        public IActionResult Overview()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                string username = User.GetDisplayName();
+                string name = User.GetNameIdentifierId();
+                Console.WriteLine($"User {username} with ID {name} is authenticated");
+            }
             return View();
         }
 
