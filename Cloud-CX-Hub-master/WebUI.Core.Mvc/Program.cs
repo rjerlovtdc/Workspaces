582cf3c98e26d5e5612bdb27415d48f634a64e5c
rasmusjerlov <rasmus.jerloev@gmail.com>
2025-03-20 10:23:39 +0100

    .

--- Cloud-CX-Hub-master/WebUI.Core.Mvc/Program.cs
+++ Cloud-CX-Hub-master/WebUI.Core.Mvc/Program.cs
@@ -5,66 +5,71 @@ using WebUI.Core.Mvc;
 using WebUI.Core.Mvc.Models;
 using CookieAuthenticationDefaults = Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults;
 
-var config = new ConfigurationBuilder()
-    .AddUserSecrets<Program>()
-    .Build();
-var clientSecret = config["AzureAd:ClientSecret"];
-var clientId = config["AzureAd:ClientId"];
-Console.WriteLine();
-
-var builder = WebApplication.CreateBuilder();
-Init.Run();
-Console.WriteLine("Initializing...");
-Console.WriteLine();
-
-builder.Services.AddAuthentication(options =>
+internal class Program
+{
+    public static void Main(string[] args)
     {
-        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = MicrosoftAccountDefaults.AuthenticationScheme;
-    })
-    .AddMicrosoftAccount(microsoftOptions =>
-{
-    microsoftOptions.ClientId = clientId;
-    microsoftOptions.ClientSecret = clientSecret;
-}).AddCookie(options =>
-{
-    options.Cookie.HttpOnly = true;
-});
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+        var clientSecret = config["AzureAd:ClientSecret"];
+        var clientId = config["AzureAd:ClientId"];
+
+        var builder = WebApplication.CreateBuilder();
+        Init.Run();
+        Console.WriteLine("Initializing...");
+
+        builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = MicrosoftAccountDefaults.AuthenticationScheme;
+            })
+            .AddMicrosoftAccount(microsoftOptions =>
+            {
+                microsoftOptions.ClientId = clientId;
+                microsoftOptions.ClientSecret = clientSecret;
+            }).AddCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+            });
 
 // Program.cs
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("AdminOnly", policy =>
-        policy.RequireClaim("AccessRights", AccessRights.Admin.ToString(), AccessRights.Owner.ToString()));
+        //Adding policy with claims
+        builder.Services.AddAuthorization(options =>
+        {
+            options.AddPolicy("AdminOnly", policy =>
+                policy.RequireClaim("AccessRights", AccessRights.Admin.ToString(), AccessRights.Owner.ToString()));
 
-    options.AddPolicy("ManagerOrAdmin", policy =>
-        policy.RequireClaim("AccessRights", AccessRights.Manager.ToString(), AccessRights.Admin.ToString(),
-            AccessRights.Owner.ToString()));
+            options.AddPolicy("ManagerOrAdmin", policy =>
+                policy.RequireClaim("AccessRights", AccessRights.Manager.ToString(), AccessRights.Admin.ToString(),
+                    AccessRights.Owner.ToString()));
     
-});
+        });
 
-builder.Services.AddControllersWithViews();
+        builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+        var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-}
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthorization();
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
 // app.UseAuthentication();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=HomePage}");
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=HomePage}");
 
-Console.WriteLine("Finished building app");
-Console.WriteLine("App running....");
-app.Run();
+        Console.WriteLine("Finished building app");
+        Console.WriteLine("App running....");
+        app.Run();
+    }
+}
